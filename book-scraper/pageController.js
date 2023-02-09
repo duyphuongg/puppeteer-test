d52ccfd4838a1be399353b7709f8b4dbe89ddfa2
phuongbd <phuongbd@fireapps.vn>
2023-02-09 15:32:42 +0700

    add demo crawl

---
+++ book-scraper/pageController.js
@@ -0,0 +1,16 @@
+const pageScraper = require('./pageScraper');
+async function scrapeAll(browserInstance){
+	let browser;
+	try{
+		browser = await browserInstance;
+		let scrapedData = {};
+		// Call the scraper for different set of books to be scraped
+		scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
+		await browser.close();
+		console.log(scrapedData)
+	}
+	catch(err){
+		console.log("Could not resolve the browser instance => ", err);
+	}
+}
+module.exports = (browserInstance) => scrapeAll(browserInstance)
